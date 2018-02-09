4a1e1734487dccc49b7b77681df273d137568941
Unknown <saikishore.eppalagudem@gmail.com>
2018-02-09 10:58:53 -0400

    Code refactoring. Added comments to code.

--- app/src/main/java/com/example/saikishoreeppalagudem/pwdtester/MainActivity.java
+++ app/src/main/java/com/example/saikishoreeppalagudem/pwdtester/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.saikishoreeppalagudem.pwdtester;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String pwd;
+    public static Boolean pwdStrength = false;
+    public static TextView tv;
+    public static EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        tv = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        editText.getText();
+       }
+
+    public void btnClick(View view) {
+        //Method to handle button click event.
+        pwd = String.valueOf(editText.getText());
+        Log.d("pwd", pwd);
+        pwdStrength = Validator.validatePassword(pwd);
+        Log.d("logmsg", pwdStrength.toString());
+        if (pwdStrength) {
+                tv.setTextColor(Color.parseColor("#0ab598"));
+                tv.setText("Strong");
+                editText.setBackgroundColor(Color.parseColor("#0ab598"));
+            Log.d("logmsg", "btnClick: true");
+        }
+        else
+        {
+            tv.setTextColor(Color.parseColor("#e00b0b"));
+            tv.setText("Weak");
+            editText.setBackgroundColor(Color.parseColor("#e00b0b"));
+            Log.d("logmsg", "btnClick: false");
+        }
+
+
     }
 }
--- app/src/test/java/com/example/saikishoreeppalagudem/pwdtester/ValidatorUnitTest.java
+++ app/src/test/java/com/example/saikishoreeppalagudem/pwdtester/ValidatorUnitTest.java
@@ -14,21 +14,17 @@ public class ValidatorUnitTest {
     public void PwdIsOK(){
         Validator v = new Validator();
         Boolean a;
-        MainActivity mainActivity = new MainActivity();
-        password = mainActivity.pwd;
-        if (v.validatePassword(password) == true)
+
+        //To check whether the Validator class is functioning as desired.
+        //I've passed a password string "Text1234$$" to check the functionality.
+        if (v.validatePassword("Text1234$$") == true)
         {
             a = true;
-
         }
         else
         {
             a = false;
-
         }
-
         assertTrue("True" , a);
-
     }
-
 }
